a00e2019b0b49cf0f66e43c36fa2b0ce67c0e209
priyal18 <priyal930gupta@gmail.com>
2020-12-09 23:10:34 +0530

    first Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Priyal',
+        username: 'Brot',
+        email: 'priyal@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Shivam',
+        username:'Antonica',
+        email: 'shivam@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Rajni',
+        username: 'cook',
+        email: 'rajni@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Surinder',
+        username: 'money',
+        email: 'surinder@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Rupali',
+        username: 'rupa',
+        email: 'rupali@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Priyal',
+        username: 'Brot',
+        email: 'priyal@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Shivam',
+        username:'Antonica',
+        email: 'shivam@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Rajn',
+        username: 'cook',
+        email: 'rajni@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Surinde',
+        username: 'money',
+        email: 'surinder@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Rupal',
+        username: 'rupa',
+        email: 'rupali@gmail.com'
+    }
+]
